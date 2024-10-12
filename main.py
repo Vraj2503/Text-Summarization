7985759e08006a1219dd2603f14a766888075729
Vraj Patel <vrajpatel250304@gmail.com>
2024-10-13 00:20:06 +0530

    validation pipeline setup done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from wordSummerizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline 
+from wordSummerizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from wordSummerizer.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -7,6 +8,20 @@ try:
     logger.info(f"Started {STAGE_NAME}")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f"Completed {STAGE_NAME}")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f"Started {STAGE_NAME}")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f"Completed {STAGE_NAME}")
 
 except Exception as e:
--- src/wordSummerizer/pipeline/stage_01_data_ingestion.py
+++ src/wordSummerizer/pipeline/stage_01_data_ingestion.py
@@ -6,7 +6,7 @@ class DataIngestionTrainingPipeline:
     def __init__(self):
         pass
 
-    def main():
+    def main(self):
         config = ConfigurationManager()
         data_ingestion_config = config.get_data_ingestion_config()
         data_ingestion = DataIngestion(config = data_ingestion_config)
